d87e3312b5de3d6999308c72d5d6179b801e5030
Vonetta <vonettastevenson@gmail.com>
2017-11-26 13:00:35 -0800

    JSON object

---
+++ main.js
@@ -0,0 +1,15 @@
+var myFaveColors =["orange", "blue", "black"];
+
+var thePets = [
+    {
+        "name": "garfield",
+        "species": "cat",
+        "faveFood": "tuna"}, 
+    {
+        "name": "barky",
+        "species": "dog",
+        "faveFood": "carrots"}
+    
+];
+
+thePets[1].faveFood
