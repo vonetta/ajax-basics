2ccef72870bebfb83cc4e3c9385969475543e997
Vonetta <vonettastevenson@gmail.com>
2017-11-26 13:32:47 -0800

    outputting json with a button click

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-var myFaveColors =["orange", "blue", "black"];
+var pageCounter = 1;
 
-var thePets = [
-    {
-        "name": "garfield",
-        "species": "cat",
-        "faveFood": "tuna"}, 
-    {
-        "name": "barky",
-        "species": "dog",
-        "faveFood": "carrots"}
+var animalContainer = document.getElementById("animal-info");
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');//gets data from 
+    ourRequest.onload = function(){
+    var ourData = JSON.parse(ourRequest.responseText);
+renderHTML(ourData);
+    };
     
-];
+    ourRequest.send();
+    pageCounter++;
 
-thePets[1].faveFood
+    if(pageCounter > 3){
+        btn.classList.add("hide-me");
+    }
+});
+
+function renderHTML(data) {
+var htmlString = "";
+
+for(var i = 0; i < data.length; i++){
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + "</p>";
+}
+animalContainer.insertAdjacentHTML('beforeend', htmlString)
+}
